55539a1f32f5ad30721a8767497d9b8e841c7227
Lee Henkel <lhenkel@gmail.com>
2020-04-16 11:11:41 -0600

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "pk_test_eNulLBF50JhKaMBRLoQjsfr200jmKB41B1",
     s3: {
         REGION: "us-east-2",
         BUCKET: "com.henkelsoft.notes"
